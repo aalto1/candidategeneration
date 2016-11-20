cf4b6d4422645fc13fe43eb8712cc036e1fe5ede
aalto1 <aalto@null.net>
2016-11-19 20:53:08 -0500

    pom: shade

--- src/main/java/PredictiveIndex/WWWMain.java
+++ src/main/java/PredictiveIndex/WWWMain.java
@@ -46,8 +46,8 @@ public class WWWMain extends WWW {
         //ExternalSort.massiveBinaryMerge(new File(dBigramIndex+rawI2),dBigramIndex+sortedI2);
         //sortComplexRanking();
         //getFQT(10);
-        System.out.println("klkn");
-        System.exit(1);
+        //System.out.println("klkn");
+        //System.exit(1);
         //getId2TermMap();
 
         InvertedIndex i2;
